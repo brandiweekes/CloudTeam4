c85999edb1239d7935630297e9f4a1e464264acf
brandiweekes <brandiweekes@gmail.com>
2018-02-19 20:20:28 -0800

    stubbing the namespace directory

--- AwsGroupProj1/AwsGroupProj1/Namenode.cs
+++ AwsGroupProj1/AwsGroupProj1/Namenode.cs
@@ -8,6 +8,36 @@ namespace AwsGroupProj1
 {
     class Namenode
     {
+        NS_File_Info file_info;
+        Dictionary<string, NS_File_Info> NN_namespace;
 
+
+
+
+        class NS_File_Info
+        {
+            public string Filename { get; set; }
+            public int Replication_Factor { get; }
+
+            private List<int> block_ids;
+            
+
+            public NS_File_Info(string fn)
+            {
+                Filename = fn;
+                Replication_Factor = 3;
+                this.block_ids = new List<int>();
+            }
+
+            public void Add_BlockIDs (List<int> ids)
+            {
+                foreach(var i in ids)
+                {
+                    this.block_ids.Add(i);
+                }
+            }
+            
+
+        }
     }
 }
