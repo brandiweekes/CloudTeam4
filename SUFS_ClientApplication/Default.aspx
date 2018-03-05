366d37d327193cc4154c1912bbf06a7a63a33f82
jasbir90 <kaurj7@seattleu.edu>
2018-03-04 17:51:01 -0800

    Added Client code
    Client Code-
    To retrive s3 file size
    to retrieve blocks of byte range

---
+++ SUFS_ClientApplication/Default.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SUFS_ClientApplication._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head id="Head1" runat="server">
+    <title>My AWS Enabled Application - SUFS_ClientApplication</title>
+    <link rel="stylesheet" href="styles/styles.css" type="text/css" media="screen" charset="utf-8"/>
+</head>
+
+<body>
+    <!--
+<div id="content" class="container">
+    <div class="section grid grid5 s3">
+        <h2>Create File</h2>
+        <input text=""/>
+    </div>
+    <div class="section grid grid5 sdb">
+        <h2>Amazon SimpleDB Domains:</h2>
+        <ul>
+            <asp:Label ID="sdbPlaceholder" runat="server"></asp:Label>
+                
+        </ul>
+    </div>
+
+    <div class="section grid grid5 gridlast ec2">
+        <h2>Amazon EC2 Instances:</h2>
+        <ul>
+            <asp:Label ID="ec2Placeholder" runat="server"></asp:Label>
+
+        </ul>
+    </div>
+</div> -->
+    <h1><center> <b> <u>SU File System </u></b></center></h1>
+    <form id="form1" runat="server">
+        <p>
+            <label>Enter Bucket Name:</label> <br/>
+            <input type= "text" name="bucketName"/> <br/><br/>
+            <label>Enter Key Name:</label> <br/>
+            <input type= "text" name="keyName"/> <br/><br/>
+            <asp:Button id="button1" runat="server" Text="Create File" OnClick="button1Clicked" />
+        </p>
+        <br/>
+    </form>
+   </body>
+</html>
---
+++ SUFS_ClientApplication/S3Client.cs
@@ -0,0 +1,70 @@
+﻿using Amazon.S3;
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Amazon;
+using Amazon.EC2;
+using Amazon.EC2.Model;
+using Amazon.SimpleDB;
+using Amazon.SimpleDB.Model;
+using Amazon.S3.Model;
+
+namespace SUFS_ClientApplication
+{
+    public class S3Client
+    {
+        //string text = System.IO.File.ReadAllText(@"C:\Users\Public\TestFolder\WriteText.txt");
+        static string secretKey = System.IO.File.ReadLines(@"Pass").ElementAt(1);
+        static string accessKey = System.IO.File.ReadLines(@"Pass").First();
+
+        //string bucketName = "wordcount-kaurj7";
+        //long bucketSize = -1;
+
+        AmazonS3Config config1 = new AmazonS3Config();
+        //config1.ServiceURL = "objects.dreamhost.com";
+
+        AmazonS3Client s3Client = new AmazonS3Client(
+                 accessKey, secretKey
+                );
+        public long getBucketSize(string bucketName, string keyName)
+        {
+            GetObjectRequest request = new GetObjectRequest
+            {
+                BucketName = bucketName,
+                Key = keyName,
+                
+            };
+            long size;
+            using (GetObjectResponse response = s3Client.GetObject(request))
+            {
+                 size = response.ContentLength;
+            }
+            return size;
+
+        }
+        public void retrieveObjectFromS3(string bucketName, string keyName, int startByte, int endByte)
+        {
+            GetObjectRequest request = new GetObjectRequest
+            {
+                BucketName = bucketName,
+                Key = keyName,
+                ByteRange = new ByteRange(startByte, endByte)
+            };
+
+            using (GetObjectResponse response = s3Client.GetObject(request))
+            {
+                string dest = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), keyName);
+                if (!File.Exists(dest))
+                {
+                    response.WriteResponseStreamToFile(dest);
+                }
+            }
+
+
+        }
+    }
+}
