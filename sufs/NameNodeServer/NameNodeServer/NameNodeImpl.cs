320f4b4729794a22d656b62834ede24c1044c830
Vincent Yang <yangv2@seattleu.edu>
2018-03-06 17:05:39 -0800

    "NNImpl Comment"

--- sufs/NameNodeServer/NameNodeServer/NameNodeImpl.cs
+++ sufs/NameNodeServer/NameNodeServer/NameNodeImpl.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using Amazon.EC2.Model.Internal.MarshallTransformations;
+using Microsoft.SqlServer.Server;
 using Sufs; //Project -> Add Reference -> Project -> select project
 
 namespace NameNodeServer
 {
     class NameNodeImpl : NameNode.NameNodeBase
     {
+        public bool HeartBeat(string DNid)
+        {
+            bool ack = true;
+            // 1. Save DNid to HealthRecord
+            // 2. set timer
+            // 3. check timer
+            return ack;
+        }
+    }
+
+    class HealthRecords : NameNode.NameNodeBase
+    {
+        public string DNid { get; set; }
+        public Timer = new Timer(3000);
+        public bool isAlive { get; set; }
     }
 }
