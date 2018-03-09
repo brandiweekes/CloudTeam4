0f6649855429a632ba933d8ddc6c977efe7c54fc
Neha <mundada.neha7@gmail.com>
2018-03-08 19:23:50 -0800

    DataNode Server Implementation

--- sufs/DataNodeServer/DataNodeServer/DataNodeImpl.cs
+++ sufs/DataNodeServer/DataNodeServer/DataNodeImpl.cs
@@ -11,7 +11,17 @@ namespace DataNodeImpl
 {
     class DataNodeServer : FileHandling.FileHandlingBase
     {
-        public override Task<WriteResponse> WriteFile(WriteRequest request, ServerCallContext context)
+        public override Task<WriteResponse> WriteFile(IAsyncStreamReader<WriteRequest> requestSream, ServerCallContext context)
+        {
+
+        }
+
+        public override Task<HealthResponse> HealthCheck(IAsyncStreamReader<HealthRequest> requestSream, ServerCallContext context) 
+        {
+            //if reponse is 0 then replicate else delete the block
+        }
+
+        public override Task<ReplicateResponse> ReplicateBlock(IAsyncStreamReader<ReplicateRequest> requestSream, ServerCallContext context)
         {
 
         }
